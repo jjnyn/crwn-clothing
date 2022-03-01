c5c1fbad14ebe3f2a2a2d05e3a788abe60489ee2
Jiajen Yuan <jiajen.yuan@agoda.com>
2022-03-01 13:54:13 +0800

    add Router and withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => {
+const HatsPage = () => (
   <div>
-    <h1>HATS PAGE</h1>
-  </div>;
-};
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
